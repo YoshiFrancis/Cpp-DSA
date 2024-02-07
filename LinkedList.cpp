ef92f6d644aca8d4e233fc49666d1e7df17a8aa6
Francis Ozua <fozua@Franciss-MacBook-Air.local>
2024-02-07 00:10:22 -0500

    Moved linked list over to the heap from the stack. Capabilities of removing and inserting at indexes at O(n) time. Also has capabilites of printing out list, adding new tail, and adding new head.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cassert>
 
 template <typename T>
 struct Node {
@@ -18,41 +18,30 @@ public:
         Node<T>* currNode {};
         for (int i = 0; i < length; i++) {
             if (i == 0) {
-                Node<T>* newHead {};
-                newHead->val = array[i];
-                newHead->next = nullptr;
-
-                m_head = newHead;
+                m_head = new Node<T>{array[i], nullptr};
                 currNode = m_head;
             }
             else if (i == length - 1) {
-                Node<T>* newTail {};
-                newTail->val = array[i];
-                newTail->next = nullptr;
-                m_tail = newTail;
+                m_tail = new Node<T>{array[i], nullptr};;
+                currNode->next = m_tail;
             }
             else {
-                Node<T>* nextNode {};
-                nextNode->val = array[i];
-                nextNode->next = nullptr;
-                currNode->next = nextNode;
+                currNode->next = new Node<T>{array[i], nullptr};;
                 currNode = currNode->next;
             }
         }
     }
 
     const void addHead(T val) {
-        Node<T>* newHead = { val, m_head };
-        m_head = newHead;
-        if (m_tail == nullptr) m_tail = newHead;
+        m_head = new Node<T>{val, m_head};;
+        if (m_tail == nullptr) m_tail = m_head;
         m_length++;
     } 
 
     const void addTail(T val) {
-        Node<T>* newTail = { val };
-        m_tail->next = newTail;
-        m_tail = newTail;
-        if (m_head == nullptr) m_head = newTail;
+        m_tail->next = new Node<T>{val, nullptr};;
+        m_tail = m_tail->next;
+        if (m_head == nullptr) m_head = m_tail;
         m_length++;
     } 
 
@@ -66,10 +55,43 @@ public:
         std::cout << "]\n";
     }
 
+    const int getLength() const {
+        return m_length;
+    }
+
+    const void insertAtIndex(int idx, T val) {
+        assert(idx < m_length && idx >= 0);
+        if (idx == 0) addHead(val);
+        else if (idx == m_length - 1) addTail(val);
+        else {
+            // O(N) time to insert
+            Node<T>* iterator { getNode(idx - 1) };
+            Node<T>* newNode = new Node<T>{ val, iterator->next };
+            iterator->next = newNode;
+            m_length++;
+        }
+    }
+
+    const void removeIndex(int idx) {
+        assert(idx < m_length && idx >= 0);
+        Node<T>* iterator { getNode(idx - 1) };
+        Node<T>* removedNode { iterator->next };
+        iterator->next = removedNode->next;
+        delete removedNode;
+    }
+
 private:
     Node<T>* m_head {};
     Node<T>* m_tail {};
     int m_length {};
+
+    Node<T>* getNode(int idx) const {
+        assert(idx < m_length && idx >= 0);
+        // O(N) time to find
+        Node<T>* iterator { m_head };
+        for (int i {0}; i < idx; i++) iterator = iterator->next;
+        return iterator;
+    }
 
 };
 
@@ -77,6 +99,19 @@ private:
 int main() {
     int array[5] {1, 2, 3, 4, 5};
     LinkedList<int> myList = {array, 5};
+    myList.printLinkedList();
+    myList.addHead(1);
+    myList.addHead(1);
+    myList.addHead(1);
+    myList.addHead(1);
+    myList.addTail(0);
+    myList.addTail(0);
+    myList.addTail(0);
+    myList.printLinkedList();
+    myList.removeIndex(5);
+    myList.insertAtIndex(5, 7);
+    myList.insertAtIndex(5, 8);
+    myList.insertAtIndex(5, 9);
     myList.printLinkedList();
     return 0;
 }
